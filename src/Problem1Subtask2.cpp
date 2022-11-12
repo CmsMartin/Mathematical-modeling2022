fb68ef8623820dcb2bbd85ac0e8787d39e4ab571
CmsMartin <Chenmingshuo0211@outlook.com>
2022-11-12 15:23:46 +0800

    Add

---
+++ src/Problem1Subtask2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+const ll INF = 1e18;
+
+int N, M;
+ll G[510][510], Dist[510][510], Rank[510][510], W[510];
+
+void CenTer_Point(int &u, int &v, double &x) {
+    for (int k = 1; k <= N; k++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                Dist[i][j] = min(Dist[i][j], Dist[i][k] + Dist[k][j]);
+            }
+        }
+    }
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) Rank[i][j] = j;
+        for (int j = 1; j <= N; j++) {
+            for (int k = j + 1; k <= N; k++) {
+                if (Dist[i][Rank[i][j]] > Dist[i][Rank[i][k]]) {
+                    swap(Dist[i][Rank[i][j]], Dist[i][Rank[i][k]]);
+                }
+            }
+        }
+    }
+
+    double Ans = 1e18;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            if (i == j || G[i][j] == INF) continue;
+            int p = Rank[i][N];
+            ll Temp = W[i] * Dist[i][p];
+            if (Ans > Temp) {
+                Ans = Temp;
+                u = i;
+                v = j;
+                x = 0.00;
+            }
+            for (int k = N - 1; k >= 1; k--) {
+                int t = Rank[i][k];
+                if (Dist[j][t] > Dist[j][p]) {
+                    Temp = W[i] * (Dist[i][t] + Dist[j][p] + G[i][j]);
+                    if (Ans > Temp) {
+                        Ans = Temp;
+                        u = i;
+                        v = j;
+                        x = (Dist[j][p] + Dist[i][j] - Dist[i][t]) / 2.00;
+                    }
+                    p = t;
+                }
+            }
+        }
+    }
+
+    return;
+}
